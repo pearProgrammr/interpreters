db4c9daaf9b45a6865da9ec586e0552f49d8835d
Erik Schmauss <schmauss@pdx.edu>
2017-10-03 19:54:34 -0700

    Untyped: fixing style, no functional change

--- untyped/Eval.hs
+++ untyped/Eval.hs
@@ -5,17 +5,6 @@ import Tokens
 
 evalUntyped input = show $ eval input []
 
--- TODO: clean this up. This can be simplified with the show function
-printTerm :: Term -> String
-printTerm (Variable name)    = "Var " ++ name ++ " "
-printTerm (Lambda name term) = "Lambda " ++ name ++ " " ++ (printTerm term) ++ " "
-printTerm (App t1 t2)        = "App " ++ (printTerm t1) ++ (printTerm t2) ++ " "
-printTerm (Brack term)       =  "( " ++ (printTerm term) ++ " ) "
-printTerm (Const val)        = "Constant Value " ++ (show val)
-printTerm (Add e1 e2)        = (printTerm e1) ++ " + " ++ (printTerm e2) ++ " "
-printTerm (Sub e1 e2)        = (printTerm e1) ++ " - " ++ (printTerm e2) ++ " "
-printTerm (Let n e b)        = "Let " ++ n ++ " = " ++ printTerm e ++ " in " ++ printTerm b
-
 data Val = Num Int
          | Func Name Term Env
          deriving Show
@@ -27,7 +16,7 @@ eval (Variable n) env = case lookup n env of
                             Just val -> val
                             Nothing  -> error ("Unbound variable " ++ n)
 
-eval (Lambda name body) env = Func name body env
+eval (Lambda n body) env = Func n body env
 
 eval (Let n expr body) env = eval body ((n, eval expr env):env)
 
@@ -37,13 +26,26 @@ eval (App body param) env =
          _                -> error ("Function required at the left-side of application")
 
 eval (Const numval) env = Num numval
-eval (Add e1 e2) env = evalMathOp e1 e2 env (+)
-eval (Sub e1 e2) env = evalMathOp e1 e2 env (-)
-eval (Mul e1 e2) env = evalMathOp e1 e2 env (*)
-eval (Div e1 e2) env = evalMathOp e1 e2 env quot
 
-evalMathOp :: Term -> Term -> Env -> (Int -> Int -> Int) -> Val
-evalMathOp e1 e2 env op =
+eval (MathOp op e1 e2) env = 
     case (eval e1 env, eval e2 env) of
-         (Num val1, Num val2) -> Num (val1 `op` val2)
+         (Num val1, Num val2) -> Num (applyOp op val1 val2)
          _                    -> error ("bad math")
+
+applyOp Add = (+) 
+applyOp Sub = (-)
+applyOp Mul = (*)
+applyOp Div = quot
+
+
+printTerm :: Term -> String
+printTerm (Variable n)       = "Var " ++ n ++ " "
+printTerm (Lambda n t)       = "Lambda " ++ n ++ " " ++ printTerm t ++ " "
+printTerm (App t1 t2)        = "App " ++ printTerm t1 ++ printTerm t2 ++ " "
+printTerm (Brack t)          =  "( " ++ printTerm t ++ " ) "
+printTerm (Const val)        = "Constant Value " ++ show val
+printTerm (MathOp Add e1 e2) = printTerm e1 ++ " + " ++ printTerm e2 ++ " "
+printTerm (MathOp Sub e1 e2) = printTerm e1 ++ " - " ++ printTerm e2 ++ " "
+printTerm (MathOp Mul e1 e2) = printTerm e1 ++ " * " ++ printTerm e2 ++ " "
+printTerm (MathOp Div e1 e2) = printTerm e1 ++ " / " ++ printTerm e2 ++ " "
+printTerm (Let n e b)        = "Let " ++ n ++ " = " ++ printTerm e ++ " in " ++ printTerm b
--- untyped/Grammar.y
+++ untyped/Grammar.y
@@ -35,15 +35,17 @@ Term  : var                      { Variable $1 }
       | Term Term                { App $1 $2 }
       | '(' Term ')'             { $2 }
       | constInt                 { Const $1 }
-      | Term '+' Term            { Add $1 $3 }
-      | Term '-' Term            { Sub $1 $3 }
-      | Term '*' Term            { Mul $1 $3 }
-      | Term '/' Term            { Div $1 $3 }
+      | Term '+' Term            { MathOp Add $1 $3 }
+      | Term '-' Term            { MathOp Sub $1 $3 }
+      | Term '*' Term            { MathOp Mul $1 $3 }
+      | Term '/' Term            { MathOp Div $1 $3 }
 
 
 {
 parseError :: [Token] -> a
 parseError = error "Parse error"
+
+
 
 type Name = String
 data Term = Variable Name
@@ -51,10 +53,10 @@ data Term = Variable Name
           | Lambda Name Term
           | App Term Term
           | Brack Term
-          | Add Term Term
-          | Sub Term Term
-          | Mul Term Term
-          | Div Term Term
+          | MathOp Op Term Term
           | Let Name Term Term
           deriving Show
+
+data Op = Add | Sub | Mul | Div
+        deriving Show
 }
