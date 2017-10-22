24ab4748a69aeabf0d50ca220b598523f89e6328
Erik Schmauss <schmauss@pdx.edu>
2017-10-21 18:23:07 -0700

    simplyTyped: moving function, no functional change, no pun intended

--- simplyTyped/Eval.hs
+++ simplyTyped/Eval.hs
@@ -65,14 +65,3 @@ eval (If e1 e2 e3) env =
          _                     -> error ("if predeicate is not a boolean value")
 
 
--- use this for debugging
-printTerm :: Term -> String
-printTerm (Var n)       = "Var " ++ n ++ " "
-printTerm (Lambda n t)       = "Lambda " ++ n ++ " " ++ printTerm t ++ " "
-printTerm (Apl t1 t2)        = "Apl " ++ printTerm t1 ++ printTerm t2 ++ " "
-printTerm (IntConst val)        = "Constant Value " ++ show val
-printTerm (MathOp Add e1 e2) = printTerm e1 ++ " + " ++ printTerm e2 ++ " "
-printTerm (MathOp Sub e1 e2) = printTerm e1 ++ " - " ++ printTerm e2 ++ " "
-printTerm (MathOp Mul e1 e2) = printTerm e1 ++ " * " ++ printTerm e2 ++ " "
-printTerm (MathOp Div e1 e2) = printTerm e1 ++ " / " ++ printTerm e2 ++ " "
-printTerm (Let n e b)        = "Let " ++ n ++ " = " ++ printTerm e ++ " in " ++ printTerm b
