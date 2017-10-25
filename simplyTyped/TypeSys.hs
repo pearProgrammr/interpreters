3167d790c7403fa3660871a64bde3be68f619228
Erik Schmauss <schmauss@pdx.edu>
2017-10-24 22:56:22 -0700

    SimplyTyped: prototyped "error monad" type inference

--- simplyTyped/TypeSys.hs
+++ simplyTyped/TypeSys.hs
@@ -174,6 +174,92 @@ infer2 env (If c e1 e2)
     (s3, t3) <- infer2 (applySubstToEnv s2 env)  e2
     return (unify (applySubst s3 t2) t3 @@ s3 @@ s2 @@ s1, t3)
 
+-- monadic style (result)
+data Res t = Err String | Answer t
+
+instance Functor Res where
+  fmap f (Answer t) = Answer (f t)
+  fmap f (Err s) = Err s
+
+instance Applicative Res where
+  pure x = Answer x
+  -- (<*>) :: Res (a -> b) -> Res a -> Res b
+  f <*> x = case f of
+              Answer f' -> fmap f' x
+              Err s -> Err s
+
+instance Monad Res where
+  -- (>>=) :: Res a -> (a -> Res b) -> Res b
+  x >>= f = case x of
+              Answer x' -> f x'
+              Err s -> Err s
+
+getRes :: Res (Subst, Type, Int) -> Either (Subst, Type, Int) [Char]
+getRes (Answer (x,y,z)) = Left (x,y,z)
+getRes (Err s) = Right s
+
+
+--unification algorithm
+unify2 :: Type -> Type -> Res Subst
+unify2 TInt TInt = return nullSubst
+unify2 TBool TBool = return nullSubst
+unify2 (TVar v) (TVar w) = return [(v, TVar w) | v /= w]
+unify2 (TVar v) t = varBind2 v t
+unify2 t (TVar v) = varBind2 v t
+unify2 (TFun t1 t2) (TFun t1' t2')
+  = do
+    s1 <- unify2 t1 t1'
+    s2 <- unify2 (applySubst s1 t2) (applySubst s1 t2')
+    return (s2 @@ s1)
+
+unify2 t1 t2 = Err ("Cannot unify "++ show t1 ++ " with " ++ show t2)
+
+varBind2 :: TyVar -> Type -> Res Subst
+varBind2 v t = if (v `elem` vars t)
+                 then Err "Occurs check fails!"
+                 else return [(v, t)]
+              where vars (TVar v)   = [v]
+                    vars TInt       = []
+                    vars TBool      = []
+                    vars (TFun l r) = vars l ++ vars r
+
+infErr :: Env -> Term -> Int -> Res (Subst, Type, Int)
+infErr env (Var v) n
+  = return (nullSubst, envLookup v env, n)
+
+infErr env (Lambda v e) n
+  = do
+    let (u, n1) = newVar n
+    case infErr ((v, u):env) e n1 of
+      Answer (s, t, n2) -> return (s, TFun (applySubst s u) t, n2)
+      Err s -> Err s
+
+infErr env (Apl l r) n -- gross
+  = do
+    case infErr env l n of
+      Err s -> Err s
+      Answer (s1, t1, n1) -> case infErr (applySubstToEnv s1 env) r n1 of
+                               Err s' -> Err s'
+                               Answer (s2, t2, n2) -> do
+                                                      let (v, n3) = newVar n2
+                                                      s3 <- unify2 (applySubst s2 t1) (TFun t2 v)
+                                                      return (s3 @@ s2 @@ s1, applySubst s3 v, n3)
+infErr env (IntConst num) n
+  = return (nullSubst, TInt, n)
+
+infErr env (MathOp op num1 num2) n
+  = do
+    case infErr env num1 n of
+      Err s -> Err s
+      Answer (s1, t1, n1) -> do
+                             s1' <- unify2 t1 TInt
+                             case infErr (applySubstToEnv (s1' @@ s1) env) num2 n1 of
+                               Err s' -> Err s'
+                               Answer (s2, t2, n2) -> do
+                                                      s2' <- unify2 t2 TInt
+                                                      return (s2' @@ s2 @@ s1' @@ s1, TInt, n2)
+
+
 -- functional style
 newVar n = (TVar (TyVar n), n+1)
 
@@ -237,15 +323,15 @@ infer env (If c e1 e2) n
     in (s4 @@ s3 @@ s2 @@ s1, t3, n3) -- do we need s1 here? -- the same variable in c can be used in e1 or e2
 
 
-test1 = Lambda "x" (MathOp Add (Var "x") (IntConst 1))
-test2 = Apl (Lambda "x" (MathOp Add (Var "x") (IntConst 1))) (IntConst 1)
-test3 = Lambda "x" (MathOp Add (IntConst 1) (Var "x") )
-test4 = Lambda "y" (Lambda "x" (MathOp Add (Var "y") (Var "x")))
-test5 = Apl (Lambda "y" (Lambda "x" (MathOp Add (Var "y") (Var "x")))) (IntConst 1)
-test6 = Lambda "x" (Var "x")
-test7 = Lambda "y" (Lambda "x" (Apl (Var "x") (Var "y")))
-test8 = Lambda "z" (Lambda "y" (Lambda "x" (Apl (Apl (Var "x") (Var "z")) (Var "y"))))
-test9 = Lambda "x" (Equals (Var "x") (IntConst 8))
+test01 = Lambda "x" (MathOp Add (Var "x") (IntConst 1))
+test02 = Apl (Lambda "x" (MathOp Add (Var "x") (IntConst 1))) (IntConst 1)
+test03 = Lambda "x" (MathOp Add (IntConst 1) (Var "x") )
+test04 = Lambda "y" (Lambda "x" (MathOp Add (Var "y") (Var "x")))
+test05 = Apl (Lambda "y" (Lambda "x" (MathOp Add (Var "y") (Var "x")))) (IntConst 1)
+test06 = Lambda "x" (Var "x")
+test07 = Lambda "y" (Lambda "x" (Apl (Var "x") (Var "y")))
+test08 = Lambda "z" (Lambda "y" (Lambda "x" (Apl (Apl (Var "x") (Var "z")) (Var "y"))))
+test09 = Lambda "x" (Equals (Var "x") (IntConst 8))
 test10 = Lambda "y" (Lambda "x" (Equals (Var "y") (Var "x")))
 test11 = Lambda "x" (Equals (MathOp Add (Var "x") (IntConst 1)) (BoolConst True))
 test12 = Lambda "x" (Apl test13 test13)
@@ -253,7 +339,16 @@ test13 = MathOp Add (IntConst 1) (Var "x")
 test14 = MathOp Add (Var "x") (Var "x")
 test15 = (Var "x")
 
+testList = [test01, test02, test03, test04, test05, test06, test07, test08, test09, test10, test11, test12, test13, test14, test15] 
 
 getType (_,x,_) = x
 runTest tst = prettyType (getType (infer emptyEnv tst 0))
 runTest2 tst = prettyType (snd (run (infer2 emptyEnv tst) 0))
+
+runTest3 tst = do
+               printRes $ getRes $ infErr emptyEnv tst 0
+               return ()
+
+printRes (Left (a, b, c)) = do 
+                            putStrLn (prettyType (getType (a, b, c)))
+                            return ()
